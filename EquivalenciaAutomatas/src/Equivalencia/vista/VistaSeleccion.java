666851324f0521f4fe16a2d7e16963daba0602c8
Julian Santiago Tauta Chaparro <julianstauta@gmail.com>
2018-04-15 18:47:52 -0500

    Documentacion codigo vista y control

--- EquivalenciaAutomatas/src/Equivalencia/vista/VistaSeleccion.java
+++ EquivalenciaAutomatas/src/Equivalencia/vista/VistaSeleccion.java
@@ -13,30 +13,62 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * Clase que sirve para controlar la ventana de seleccion de automatas
+ * @author JulianSantiago and LuisMiguel
+ *
+ */
 public class VistaSeleccion {
 
+	/**
+	 * Controlador de la clase
+	 */
 	private controller cont;
 	
+	/**
+	 * Combobox con los tipos de automata
+	 */
 	@FXML
 	private ComboBox comboBox;
+	
+	/**
+	 * TextField para ingresar los estimulos
+	 */
 	@FXML
 	private TextField txtEstim;
+	
+	/**
+	 * Boton para iniciar con la funcionalidad principal del programa
+	 */
 	@FXML
 	private Button butIniciar;
 	
+	/**
+	 * Constructor generico de la clase
+	 */
 	public VistaSeleccion() {
 	}
 	
+	/**
+	 * Cosntructor de la clase
+	 * @param c controller. Clase controladora de esta clase
+	 */
 	public VistaSeleccion(controller c) {
 		cont = c;
 	}
 	
+	/**
+	 * Inicializa Se encarga de inicializar algunos apectos y funciones de la parte grafica
+	 */
 	@FXML
 	public void initialize() {
 		comboBox.getItems().addAll("Mealy", "Moore");
 		butIniciar.setOnAction(e -> mostarTablas());
 	}
 	
+	/**
+	 * Maneja el evento generado al oprimir el boton iniciar
+	 */
 	public void mostarTablas() {
 		String tipo = comboBox.getValue()+"";
 		String[] estimulos = txtEstim.getText().split(",");
